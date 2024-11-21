9ed9df9a99a3592c14ed2ece41d12d300a5c8d63
LUCAS SANTANA <lhs.santanalucas@gmail.com>
2024-11-21 12:48:25 -0300

    commit inicial

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function alterarStatus(id) {
+    let jogoSelecionado = document.getElementById(`game-${id}`);
+    let imagem = jogoSelecionado.querySelector('.dashboard__item__img');
+    let botao = jogoSelecionado.querySelector('.dashboard__item__button');
+    let nomeJogo = jogoSelecionado.querySelector('.dashboard__item__name');
+    
+    if(imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
+
+    // alert(nomeJogo.textContent);
+    // alert(botao.textContent);
+    // alert(jogoSelecionado.textContent.content);
+}
